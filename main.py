30ca7a64cd342919872488ea40e9847adb91fb03
lsocpb <lukasssonik@gmail.com>
2022-10-19 18:04:48 +0200

    bullet management

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame as pg
 import game_functions as gf
 from settings import Settings
 from ship import Ship
-
+from pygame.sprite import Group
 
 def run_game():
     # initializing the game
@@ -18,11 +18,15 @@ def run_game():
     # Ship param
     ship = Ship(ai_settings, screen)
 
+    # Bullets param
+    bullets = Group()
+
     # Running the game
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 
 run_game()
